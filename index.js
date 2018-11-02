00abf9da623267f0f5491740937b9d607b22bf64
no <no>
2018-11-02 17:22:53 +0200

    works

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 const glob = require('glob-promise');
 const read = require('fs-readfile-promise');
+const write = require('fs-writefile-promise');
 const { join } = require('path');
 const mustache = require('mustache');
 const escape = require('escape-string-regexp');
 
 const START = '<!--- LERNA_PACKAGES --->';
 const END = '<!--- /LERNA_PACKAGES --->';
-const CONTENT = '[\\s\\S]';
+const CONTENT = '[\\s\\S]*';
 
 const loadTemplate = async () => {
     const filename = join(__dirname, 'default-template.mustache');
@@ -25,13 +26,14 @@ const replaceInDoc = (docContents, text) => {
         `(${escape(START)})(${CONTENT})(${escape(END)})`,
         'm'
     );
-    console.log(re);
     return docContents.replace(re, (substr, start, prevText, end) => {
         return `${start}\n${text}\n${end}`;
     });
 };
 
-const saveDoc = async () => {};
+const saveDoc = async text => {
+    return write('README.md', text);
+};
 
 const listPackages = async () => {
     const jsonFilesList = await glob('./packages/*/package.json');
@@ -47,11 +49,8 @@ const main = async () => {
     const template = await loadTemplate();
     const text = mustache.render(template, { packages });
     const docContents = await loadDoc();
-    console.log({ docContents });
     const newText = replaceInDoc(docContents, text);
-    console.log('');
-    console.log('newText:');
-    console.log(newText);
+    await saveDoc(newText);
 };
 
 main()
